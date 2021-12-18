52fa44b323e7d3cda97eccf0527f791e519e6bf7
GiWeD <>
2021-12-18 20:44:46 +0100

    fixed bug deposit for

--- contracts/LiquidSingleYieldVaultV1.sol
+++ contracts/LiquidSingleYieldVaultV1.sol
@@ -205,23 +205,6 @@ contract LiquidSingleYieldVaultV1 is ERC20, Ownable, ReentrancyGuard {
         uint256 amount = IERC20(_token).balanceOf(address(this));
         IERC20(_token).safeTransfer(msg.sender, amount);
     }
-
-    function depositFor(address token, uint _amount,address user ) public {
-        strategy.beforeDeposit();
-
-        uint256 _pool = balance();
-        IERC20(token).safeTransferFrom(msg.sender, address(this), _amount);
-        earn();
-        uint256 _after = balance();
-        _amount = _after.sub(_pool); // Additional check for deflationary tokens
-        uint256 shares = 0;
-        if (totalSupply() == 0) {
-            shares = _amount;
-        } else {
-            shares = (_amount.mul(totalSupply())).div(_pool);
-        }
-        _mint(user, shares);
-    }
 }
 
 
